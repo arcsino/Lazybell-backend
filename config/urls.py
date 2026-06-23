0aeeb3fa13698673c4391b83577603885cbfbba2
arcsino <173187432+arcsino@users.noreply.github.com>
2026-06-23 18:20:29 +0900

    管理者サイトの隠蔽化

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 from common.internal_views import InternalRemindView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{settings.ADMIN_URL}/', admin.site.urls),
     path('api/internal/remind/', InternalRemindView.as_view(), name='internal-remind'),
     path('', include('accounts.urls')),
     path('', include('groups.urls')),
